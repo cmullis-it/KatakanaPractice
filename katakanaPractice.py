1c08ae28b89bb5577b660604cc6b937952537e16
Collin Ray <cmullis.it@gmail.com>
2020-08-17 15:37:15 -0400

    Update katakanaPractice.py
    V 0.02
    
    - Separated datat from functional code to improve readability
    - Cleaned up curent 'UI' by creating spaces between prompts and responses
    - Added 'exit' command in program to exit early.

--- katakanaPractice.py
+++ katakanaPractice.py
@@ -1,46 +1,49 @@
 # Katakana Practice
 
-import random
+import random		# random is imported for shuffle to mix up the order
+import kataPairs 	# Katkana dictionary used to pair up katakana and romanji
 
-kataDict = {
-	'ア': 'a',
-	'イ': 'i',
-	'ウ': 'u',
-	'エ': 'e',
-	'オ': 'o',
-	}
+# This grabs the dictionary and creates a name for it here. 
+kataDict = kataPairs.katakanaPairs
 
+# Grabs the keys from the Dictionary and create a list, and shuffles order
 kataList = list(kataDict.keys())
 random.shuffle(kataList)
 
 
+# Prints the kana, then asks the user for input, checks against value in 
+# dictionary to see if correct
 for kata in kataList:
 	print(kata)
 	answer = input('How do you pronounce this kana?\n')
 	stripped_answer = answer.strip()
-	if stripped_answer.lower() == kataDict[kata]:
+	if stripped_answer.lower() == "exit":
+		break
+	elif stripped_answer.lower() == kataDict[kata]:
 		print("Right!\n")
 
 	else:
-		print("Wrong.\n")
+		print("Incorrect :(\n")
 	
-
-# Debug Code: print(kataDict)
-
-
-
-
 
 ##### Features that need to be added
 
 '''
-Need to be able to decide which Kana you want to be able to practice
 
-Make it so that it goes through the ones that you have missed, again.
+[x] Seperate the data from the code that runs the actual practice app
+[x] Clean up the current 'UI' 
+[x] Added 'exit' command for quick debugging right now
 
-Add a UI
+[ ] Add full list of katakana and romanji to match. 
 
-Clean up the current 'UI' 
+[ ] Add a counter so you know how many you got wrong, and how many you got right.
+
+[ ] Need to be able to decide which Kana you want to be able to practice
+
+[ ] Make it so that it goes through the ones that you have missed, again.
+
+[ ] Add a UI
+
 
 '''
 
