b12c49d5a01ce720926812b63b187abbbf78372f
Collin Ray <cmullis.it@gmail.com>
2020-08-26 09:06:12 -0400

    Update kataGUI.py

--- kataGUI.py
+++ kataGUI.py
@@ -1,41 +1,70 @@
 # GUI Practice for the application
 
 from tkinter import *
+import kataPairs
 
+# Create the master window, and name "Kat.Pract."
 master = Tk()
 master.title("Katakana Practice")
 
-prompt = "マ"
 
+# Placeholders for the prompt/ correct answer that user 
+# input is being compared against. 
+promptKana = "マ"
+promptQuestion = "Enter Romanji for this Kana"
 answer = "ma"
 
+
+# Answers for romanji the user is inputing
 rightAns = "Correct!"
 wrongAns = "Sorry, that was wrong"
 
+
+# The Kana Label that will shift through the various Kana
+lbl_KanaPrompt = Label(
+	text=promptKana,
+	height=2,
+)
+lbl_KanaPrompt.pack()
+
+lbl_KanaAnswer = Label(
+	text=answer,
+	visible=FALSE
+)
+
+# Constant prompt explaining instructions from App
+lbl_QuestionPrompt = Label(
+	text=promptQuestion
+)
+lbl_QuestionPrompt.pack()
+
+# Entry box grabbing the input from the user. 
+inputFromUser = Entry(master)
+inputFromUser.pack()
+
+################################################
+
+'''
 def checkAnswer():
+	userInput = entry.get()
+	if userInput == answer:
 		feedBack_lbl.config(text=rightAns)
+	else:
+		feedBack_lbl.config(text=wrongAns)
+'''
 
+################################################
 
-label = Label(
-	text=prompt
-	)
-label.pack()
-
+# Creates button used to check answer
 button = Button(
 	text='Check Answer',
-	command=checkAnswer,
 )
 button.pack()
 
-entry = Entry()
-entry.pack()
-
-userInput = entry.get()
-
-feedBack_lbl = Label(
-	text=''
+# Creates label used to provide Right/Wrong Feedback
+lbl_FeedBack = Label(
 )
-feedBack_lbl.pack()
+lbl_FeedBack.pack()
 
 
 
